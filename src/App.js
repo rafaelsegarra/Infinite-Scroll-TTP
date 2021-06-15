9ef8ae126e2c8025c7cb3769fbcbead06c223185
Rafael Segarra <rafaelandres54@gmail.com>
2021-06-15 19:36:15 -0400

    Finalized Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { Heading } from "./components/Heading";
+import { UnsplashImage } from "./components/UnsplashImage";
+import styled from "styled-components";
+import { stockData } from "./data";
+import React, { useState, useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import "./App.css";
 
 function App() {
+  const [images, setImage] = useState([]);
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
+  const fetchImages = () => {
+    var items = [];
+    console.log("images length ");
+
+    for (var i = 0; i < 20; i++) {
+      let number = getRandomArbitrary(0, 47);
+
+      function getRandomArbitrary(min, max) {
+        return Math.round(Math.random() * (max - min) + min);
+      }
+
+      // console.log("RANDOM NUMBER", number)
+
+      items.push(stockData[number]);
+    }
+    console.log("ITEMS INSIDE FETCHIMAGES", items);
+    setImage([...images, ...items]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Heading />
+      <div className="mainboard">
+        <div className="mainboard__container">
+          <Wrapper>
+            <Container>
+            <InfiniteScroll
+            dataLength={images.length}
+            next={fetchImages}
+            hasMore={true}
+            loader={<h1>loading...</h1>}
+          >
+            <Container>
+              {images.map((image) => (
+                <UnsplashImage url={image.images.orig.url} key={image.id} />
+              ))}
+            </Container>
+          </InfiniteScroll>
+            </Container>
+          </Wrapper>
+          
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  background-color: white;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  margin-top: 15px;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  margin: 0 auto;
+  height: 100%;
+  background-color: white;
+`;
---
+++ src/components/UnsplashImage.js
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "styled-components";
+
+
+export const UnsplashImage = ({ url }) => {
+
+  return (
+    <Wrapper>
+    <Container>
+    
+        <img src={url} alt="pin" />
+
+    </Container>
+</Wrapper>
+    )
+};
+
+
+
+const Wrapper = styled.div`
+    display: inline-flex;
+    padding: 8px;
+
+`
+
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    box-sizing: border-box;
+    cursor: pointer;
+    width: 236px;
+
+    img {
+        display: flex;
+        width: 100%;
+        cursor: zoom-in;
+        border-radius: 16px;
+        object-fit: cover;
+    }
+
+
+`
